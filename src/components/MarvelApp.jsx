b9db2fbdd57bcadaada0ce8d82ee97005f81644f
Cristian Lima <cristiannlima2@gmail.com>
2024-09-05 10:04:35 -0600

    Solución de fetch e inicio de Paginación

--- src/components/MarvelApp.jsx
+++ src/components/MarvelApp.jsx
@@ -4,35 +4,35 @@ import { useState } from "react";
 import { useEffect } from "react";
 
 export const MarvelApp = () => {
-  const [characters, setCharacters] = useState();
+  const [characters, setCharacters] = useState([])
+  const [currentpage, setCurrentPage] = useState(0)
+  const [total, setTotal] = useState()
+
 
   const time_stamp = new Date().getTime();
-  const private_api_key = "57deff65bd5eed4f04430160861c47fd116aa717";
-  const public_api_key = "2d50881383c0cb0902e6a068872019fa";
+  const private_api_key = "57deff65bd5eed4f04430160861c47fd116aa717"
+  const public_api_key = "2d50881383c0cb0902e6a068872019fa"
 
   const hash = CryptoJS.MD5(
     time_stamp + private_api_key + public_api_key
-  ).toString();
+  ).toString()
 
-  useEffect(
-    async() => {
-      const reqCharacters = async () => {
-        const resp = await fetch(
-          `https://gateway.marvel.com:443/v1/public/characters?ts=${time_stamp}&apikey=${public_api_key}&hash=${hash}`
-        );
-        const { data } = await resp.json()
-
-        setCharacters(data.results)
-        console.log(characters)
-        
-      }
-      reqCharacters()
-    },
-    [] /*depencias*/
-  );
+  useEffect(() => {
+    const reqCharacters = async () => {
+      const offset = currentpage * 20
+      const resp = await fetch(
+        `https://gateway.marvel.com:443/v1/public/characters?ts=${time_stamp}&apikey=${public_api_key}&hash=${hash}&offset=${offset}`
+      )
+      const { data } = await resp.json()
+      console.log(data)
+      setCharacters(data.results);
+      setTotal(data.total)
+    };
+    reqCharacters()
+  }, [])
 
   return <>{characters.map((char)=>(
     console.log(char)
     
-  ))}</>;
-};
+  ))}</>
+}
