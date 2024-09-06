29321489e549bfc7eb4d5980a6adaae398641269
CristiannLimaR <cristiannlima2@gmail.com>
2024-09-06 13:14:52 -0600

    navBar

--- src/components/SearchCharacter.jsx
+++ src/components/SearchCharacter.jsx
@@ -1,15 +1,17 @@
-import {useState} from "react"
+import { useState } from "react"
 
-export const SearchCharacter = ({onSearch}) => {
-    const [character, setCharacter] = useState("");
-    
-    const handleSubmit = (e) =>{
-        e.preventDefault()
-        onSearch(character)
-    }
-    return (
-      <>
-        <div className="container d-flex flex-row justify-content-center align-items-center mt-5">
+export const SearchCharacter = ({ onSearch }) => {
+  const [character, setCharacter] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    onSearch(character)
+  }
+  return (
+    <>
+      <nav className="navbar bg-dark border-bottom border-body">
+        <div className="container-fluid">
+          <img height={200} onClick={() => window.location.reload()} src="https://th.bing.com/th/id/R.93c903b1d44702c0539c513efe05cf3d?rik=5jZCxHnRN5PJVw&pid=ImgRaw&r=0"></img>
           <form
             className="d-flex"
             onSubmit={handleSubmit}
@@ -24,6 +26,9 @@ export const SearchCharacter = ({onSearch}) => {
             <input className=" btn btn-outline-success" type="submit"></input>
           </form>
         </div>
-      </>
-    );
-  };
+      </nav>
+
+
+    </>
+  );
+};
